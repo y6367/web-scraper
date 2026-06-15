9848e1e1004f205179de0b15fb19b59fb83c7035
joewah <joeyu88775@gmail.com>
2026-06-15 12:28:30 -0700

    fixed bugs

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
-import requests, time, json
+import requests, time
+import json
 from bs4 import BeautifulSoup
 
 def scrape_all_quotes():
@@ -33,20 +34,55 @@ def scrape_all_quotes():
 #     print(f"Quote #{count}: {quote.get('quote')} - {quote.get('author')}")
 #     count += 1
 
-url = "https://sdp-prem-prod.premier-league-prod.pulselive.com/api/v3/competitions/8/seasons/2025/players/stats/leaderboard?_sort=goals%3Adesc&country=&_limit=10"
-headers = {
-    "Origin": "https://www.premierleague.com",
-    "Referer": "https://www.premierleague.com/"
+url = "https://stats.nba.com/stats/leagueLeaders"
+
+params = {
+    "LeagueID": "00",
+    "PerMode": "PerGame",
+    "Scope": "S",
+    "Season": "2025-26",
+    "SeasonType": "Regular Season",
+    "StatCategory": "PTS"
 }
 
-response = requests.get(url, headers = headers)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36",
+    "Referer": "https://www.nba.com/",
+    "Origin": "https://www.nba.com",
+    "Host": "stats.nba.com"
+}
+
+response = requests.get(url, headers=headers, params=params)
 data = response.json()
 
-for players in data["data"]:
-    name = players["playerMetadata"]["name"]
-    team = players["playerMetadata"]["currentTeam"]["name"]
-    goals = players["stats"]["goals"]
-    print(f"{name} ({team}) - {goals} goals")
+# the data comes back in a weird format — headers and rows are separate
+columns = data["resultSet"]["headers"]
+rows = data["resultSet"]["rowSet"]
+
+for row in rows[:10]:  # first 10 players
+    player = dict(zip(columns, row))
+    print(f"{player['PLAYER']} - {player['PTS']} PPG")
+
+
+# url = "https://sdp-prem-prod.premier-league-prod.pulselive.com/api/v3/competitions/8/seasons/2025/players/stats/leaderboard?_sort=goals%3Adesc&country=&_limit=10"
+# headers = {
+#     "Origin": "https://www.premierleague.com",
+#     "Referer": "https://www.premierleague.com/"
+# }
+
+# response = requests.get(url, headers = headers)
+# data = response.json()
+
+nba_url = requests.get("https://www.nba.com/stats/leaders?SeasonType=Regular+Season")
+
+print(nba_url.status_code)
+print("hi")
+
+# for players in data["data"]:
+#     name = players["playerMetadata"]["name"]
+#     team = players["playerMetadata"]["currentTeam"]["name"]
+#     goals = players["stats"]["goals"]
+#     print(f"{name} ({team}) - {goals} goals")
 
 # response = requests.get("http://quotes.toscrape.com")
 # print(response.status_code)
