b2ed908609a23e5a2ac27818e99ad7ea95df9503
joewah <joeyu88775@gmail.com>
2026-06-15 18:09:39 -0700

    fixed bugs

--- weather.py
+++ weather.py
@@ -12,6 +12,7 @@ data = response.json()
 name = data[0]["name"]
 lat = data[0]["lat"]
 lon = data[0]["lon"]
+state = data[0]["state"]
 
 # print(response.text)
 # print(name)
@@ -23,6 +24,15 @@ weather_request = requests.get(weather)
 weather_data = weather_request.json()
 
 temp = weather_data["main"]["temp"]
-description = weather_data["weather"][0]["description"]
+feels_like = weather_data["main"]["feels_like"]
+humidity = weather_data["main"]["humidity"]
+wind = weather_data["wind"]["speed"]
+description = weather_data["weather"][0]["description"] 
 
-print(f"The temperature in {name} is {temp} degrees fahrenheit, it currently has {description}")
+print()
+print(f"{name}, {state}")
+print(f"Temperature: {temp}°F")
+print(f"Feels like: {feels_like}°F")
+print(f"Humidity: {humidity}%")
+print(f"Wind: {wind} mph")
+print(description.capitalize())
